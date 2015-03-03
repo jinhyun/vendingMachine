2ad343b4ca8b5940e983ae53c213f1e78c202ae2
jinhyun <free1330@gmail.com>
2015-03-03 22:03:02 +0900

    init

---
+++ src/test/TestVendingMachine.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jinhyun on 2015. 3. 3..
+ */
+public class TestVendingMachine {
+    /*
+     * 자판기의 상품이 출력된다 (상품번호와 금액을 출력한다)
+     * 사용자가 상품의 번호로 선택한다
+     * 사용자가 금액을 자판기에 넣는다(금액은 100, 500, 1000, 5000원권만 가능하다)
+     *  금액을 넣을때마다 금액이 출력된다
+     * 상품의 가격과 넣은 금액이 같거나 많을때 상품과 거스름돈을 출력한다
+     */
+}
