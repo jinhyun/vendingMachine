d50d723ba5262c61fd7a35cd9045b1cf3dbe86d6
jinhyun <free1330@gmail.com>
2015-03-04 00:47:25 +0900

    feature: 사용자가 상품의 번호로 선택한다

--- src/test/TestVendingMachine.java
+++ src/test/TestVendingMachine.java
@@ -8,27 +8,46 @@ import java.util.*;
  * Created by Jinhyun on 2015. 3. 3..
  */
 public class TestVendingMachine {
+    private VendingMachine machine;
+
+    @Before
+    public void setUp() throws Exception {
+        machine = new VendingMachine();
+        machine.setTempItemList();
+    }
+
     /*
-     * done 자판기의 상품이 출력된다 (상품번호와 금액을 출력한다)
-     * 사용자가 상품의 번호로 선택한다
+     * [v] 자판기의 상품이 출력된다 (상품번호와 금액을 출력한다)
+     * [v] 사용자가 상품의 번호로 선택한다
      * 사용자가 금액을 자판기에 넣는다(금액은 100, 500, 1000, 5000원권만 가능하다)
      *  금액을 넣을때마다 금액이 출력된다
      * 상품의 가격과 넣은 금액이 같거나 많을때 상품과 거스름돈을 출력한다
      */
     @Test
     public void viewItemList() {
-        VendingMachine vendingMachine = new VendingMachine();
-        vendingMachine.setTempItemList();
+        List<String> itemList = machine.getItemList();
+        assertTrue(itemList.size() > 0);
 
-        List<String> itemList = vendingMachine.getItemList();
-        assertTrue(itemList.size()> 0);
-
-        String resultView = vendingMachine.viewItemList();
+        String resultView = machine.viewItemList();
         assertTrue(resultView.length() > 0);
+    }
+
+    @Test
+    public void chooseItem() {
+        User user = new User();
+        VendingMachine choosedMachine = new VendingMachine();
+        choosedMachine = user.chooseItem(2);
+
+        assertEquals(machine.getItemList().get(2),
+                choosedMachine.getItemList().get(0));
     }
 
     private class VendingMachine {
         private List<String> itemList;
+
+        public void setItemList(List<String> itemList) {
+            this.itemList = itemList;
+        }
 
         public List<String> getItemList() {
             return itemList;
@@ -49,6 +68,30 @@ public class TestVendingMachine {
             itemList.add("2: 아이폰6 (125,800원)");
             itemList.add("3: 스크류바 (1,200원)");
             itemList.add("4: 맥북 (299,900원)");
+        }
+    }
+
+    private class User {
+        private int itemNo;
+
+        public int getItemNo() {
+            return itemNo;
+        }
+
+        public VendingMachine chooseItem(int itemNo) {
+            VendingMachine machine = new VendingMachine();
+            machine.setTempItemList();
+            String choosedItem = machine.getItemList().get(itemNo-0);
+
+            System.out.println("선택한 물품정보");
+            System.out.println(choosedItem);
+
+            List <String> choosedItemList = new ArrayList<>();
+            choosedItemList.add(choosedItem);
+
+            VendingMachine resultMachine = new VendingMachine();
+            resultMachine.setItemList(choosedItemList);
+            return resultMachine;
         }
     }
 }
